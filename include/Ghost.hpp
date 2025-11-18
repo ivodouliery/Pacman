a840ac8f249f97d41f385602b0a152baa224bf2c
ivodouliery <61093588+ivodouliery@users.noreply.github.com>
2025-11-18 16:41:00 +0100

    Réorganiser Ghost.hpp pour définir l'énumération GhostType et supprimer la classe Ghost

--- include/Ghost.hpp
+++ include/Ghost.hpp
@@ -1,10 +1,10 @@
+#pragma once
 #include <SFML/Graphics.hpp>
+#include <vector>
 
-class Ghost {
-
-    int x;
-    int y;
-
-    public:
-        
+enum class GhostType {
+    BLINKY,  // Red - the most aggressive, he directly chases Pac-Man
+    PINKY,   // Pink - tries to ambush you by aiming ahead of your position.
+    INKY,    // Blue - calculates a weird combo of your position + Blinky’s position, making him unpredictable.
+    CLYDE    // Orange - alternates between chasing and fleeing, chaotic but fun.
 };
