2fa60682ef5ee26fab9a98f217449f3ec7879ad2
ivodouliery <61093588+ivodouliery@users.noreply.github.com>
2025-11-25 15:56:36 +0100

    Ajouter la classe Game avec les méthodes de base pour la gestion du jeu

---
+++ include/Game.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class Game {
+public:
+    Game(); // Constructeur
+    void run(); // Lance la boucle de jeu
+
+private:
+    void processEvents(); // Gère les entrées (clavier, fermeture fenêtre)
+    void update();        // Met à jour la logique du jeu
+    void render();        // Affiche les éléments à l'écran
+
+    sf::RenderWindow mWindow; // La fenêtre SFML
+};
