fcd12112bdf1d4cc9985f4f7bc1fa9a4ee389398
ivodouliery <61093588+ivodouliery@users.noreply.github.com>
2025-12-30 14:55:14 +0100

    ajout du test : Map dimensions are correc

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,3 +1,8 @@
-int main() {
-    return 0;
+#include <catch2/catch_test_macros.hpp>
+#include "../include/Map.hpp"
+
+TEST_CASE("Map dimensions are correct", "[map]") {
+    REQUIRE(Map::MAP_WIDTH == 28);
+    REQUIRE(Map::MAP_HEIGHT == 30);
+    REQUIRE(Map::cellSize == 16.0f);
 }
