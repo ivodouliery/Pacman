7f70daf3d0309c2253693785bf2360e104b20c86
ivodouliery <61093588+ivodouliery@users.noreply.github.com>
2025-12-31 20:08:18 +0100

    Réorganisation de la classe Ghost pour initialiser correctement les sprites des yeux selon le type de fantôme.

--- src/Ghost.cpp
+++ src/Ghost.cpp
@@ -1,32 +1,40 @@
 #include "../include/Ghost.hpp"
 
-Ghost::Ghost(GhostType type) : type(type), mode(GhostMode::SCATTER) {
+Ghost::Ghost(GhostType type) : type(type), mode(GhostMode::SCATTER), sprite_eyes(texture), sprite_eyes_left_right(texture), sprite_eyes_up(texture), sprite_eyes_down(texture) {
     speed = 90.0f; // Slightly slower than Pacman
-
     if(!texture.loadFromFile("./assets/ghosts.png")) {
         throw std::runtime_error("Failed to load texture");
     }
     sprite_body = sf::Sprite(texture);
 
+    sprite_eyes_down.setTextureRect(sf::IntRect({5*eyesSize, eyesOriginY}, {eyesSize, eyesSize}));
+    sprite_eyes_up.setTextureRect(sf::IntRect({6*eyesSize, eyesOriginY}, {eyesSize, eyesSize}));
+
     switch(type) {
         case GhostType::BLINKY: 
             sprite_body.setTextureRect(sf::IntRect({0*entitySize, 0*entitySize}, {entitySize, entitySize}));
-            sprite_eyes.setTextureRect(sf::IntRect({0*eyesSize, eyesOriginY}, {eyesSize, eyesSize}));
+            sprite_eyes_left_right.setTextureRect(sf::IntRect({0*eyesSize, eyesOriginY}, {eyesSize, eyesSize}));
+            sprite_eyes = sprite_eyes_left_right;
             break;
         case GhostType::INKY: 
             sprite_body.setTextureRect(sf::IntRect({0*entitySize, 1*entitySize}, {entitySize, entitySize}));
-            sprite_eyes.setTextureRect(sf::IntRect({1*eyesSize, eyesOriginY}, {eyesSize, eyesSize}));
+            sprite_eyes_left_right.setTextureRect(sf::IntRect({1*eyesSize, eyesOriginY}, {eyesSize, eyesSize}));
+            sprite_eyes = sprite_eyes_left_right;
             break;
         case GhostType::PINKY: 
             sprite_body.setTextureRect(sf::IntRect({0*entitySize, 2*entitySize}, {entitySize, entitySize}));
-            sprite_eyes.setTextureRect(sf::IntRect({2*eyesSize, eyesOriginY}, {eyesSize, eyesSize}));
+            sprite_eyes_left_right.setTextureRect(sf::IntRect({2*eyesSize, eyesOriginY}, {eyesSize, eyesSize}));
+            sprite_eyes = sprite_eyes_left_right;
             break;
         case GhostType::CLYDE: 
             sprite_body.setTextureRect(sf::IntRect({0*entitySize, 3*entitySize}, {entitySize, entitySize}));
-            sprite_eyes.setTextureRect(sf::IntRect({3*eyesSize, eyesOriginY}, {eyesSize, eyesSize}));
+            sprite_eyes_left_right.setTextureRect(sf::IntRect({3*eyesSize, eyesOriginY}, {eyesSize, eyesSize}));
+            sprite_eyes = sprite_eyes_left_right;
             break;
     }
+
 }
+
 
 void Ghost::setPosition(float x, float y) {
     Entity::setPosition(x, y);
