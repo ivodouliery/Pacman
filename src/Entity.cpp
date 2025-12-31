ffc30d3db67fb6285d09802bc38290d36b6c7919
ivodouliery <61093588+ivodouliery@users.noreply.github.com>
2025-12-31 19:37:37 +0100

    Ajout de la classe Entity avec les méthodes pour gérer la position, la direction et la vitesse

---
+++ src/Entity.cpp
@@ -0,0 +1,30 @@
+#include "../include/Entity.hpp"
+
+Entity::Entity()
+    : position(0.f, 0.f), direction(0.f, 0.f), speed(0.f) {
+}
+
+void Entity::setPosition(float x, float y) {
+    position = {x, y - cellSize / 2.0f};
+    sprite_body.setPosition(position);
+}
+
+sf::Vector2f Entity::getPosition() const {
+    return position;
+}
+
+void Entity::setDirection(sf::Vector2f direction) {
+    this->direction = direction;
+}
+
+sf::Vector2f Entity::getDirection() const {
+    return direction;
+}
+
+void Entity::setSpeed(float speed) {
+    this->speed = speed;
+}
+
+float Entity::getSpeed() const {
+    return speed;
+}
