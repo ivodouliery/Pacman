aff21cbb1bdd3922ed89dcc29ec351ebb8b96230
ivodouliery <61093588+ivodouliery@users.noreply.github.com>
2025-12-31 20:18:23 +0100

    Amélioration de la documentation de la classe Game : ajout de commentaires Docstring pour le constructeur, la boucle de jeu, et les méthodes privées.

--- include/Game.hpp
+++ include/Game.hpp
@@ -2,16 +2,41 @@
 #include <SFML/Graphics.hpp>
 #include "Map.hpp"
 
+/**
+ * @brief Classe représentant le jeu.
+ * 
+ * Cette classe représente le jeu Pac-Man.
+ */
 class Game {
 public:
-    Game(); // Constructeur
-    void run(); // Lance la boucle de jeu
+    /**
+     * @brief Constructeur de la classe Game.
+     */
+    Game();
+    /**
+     * @brief Lance la boucle de jeu.
+     */
+    void run();
 
 private:
-    void processEvents(); // Gère les entrées (clavier, fermeture fenêtre)
-    void update();        // Met à jour la logique du jeu
-    void render();        // Affiche les éléments à l'écran
-
-    sf::RenderWindow window; // La fenêtre SFML
+    /**
+     * @brief Gère les entrées (clavier, fermeture fenêtre).
+     */
+    void processEvents();
+    /**
+     * @brief Met à jour la logique du jeu.
+     */
+    void update();
+    /**
+     * @brief Affiche les éléments à l'écran.
+     */
+    void render();
+    /**
+     * @brief La fenêtre SFML.
+     */
+    sf::RenderWindow window;
+    /**
+     * @brief La carte du jeu.
+     */
     Map grid;
 };
