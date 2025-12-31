9129149de94c0f4e5c38e025f781a203ee081ed9
ivodouliery <61093588+ivodouliery@users.noreply.github.com>
2025-12-31 19:48:58 +0100

    Ajout de la classe Ghost avec le constructeur et la méthode setPosition

--- src/Ghost.cpp
+++ src/Ghost.cpp
@@ -1,1 +1,10 @@
 #include "../include/Ghost.hpp"
+
+Ghost::Ghost(GhostType type) : type(type), mode(GhostMode::SCATTER) {
+    speed = 90.0f; // Slightly slower than Pacman
+}
+
+void Ghost::setPosition(float x, float y) {
+    Entity::setPosition(x, y);
+    sprite_eyes.setPosition({x + cellSize/2, y});
+}
