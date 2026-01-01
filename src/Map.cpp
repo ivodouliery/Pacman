8b04120e2780d19fa4a5618e1ba7e4fdc7a10bdc
Alexis Marzolin <163201587+AlexisMrz@users.noreply.github.com>
2026-01-02 00:07:35 +0100

    add affichage du score
    Added UI elements for score display and font handling.

--- src/Map.cpp
+++ src/Map.cpp
@@ -2,7 +2,7 @@
 #include <filesystem>
 #include <iostream>
 
-Map::Map(): blinky(GhostType::BLINKY), pinky(GhostType::PINKY), inky(GhostType::INKY), clyde(GhostType::CLYDE), pacman(), mapSprite(mapTexture), dotSprite(itemTexture), superDotSprite(itemTexture) {
+Map::Map(): blinky(GhostType::BLINKY), pinky(GhostType::PINKY), inky(GhostType::INKY), clyde(GhostType::CLYDE), pacman(), mapSprite(mapTexture), dotSprite(itemTexture), superDotSprite(itemTexture), m_font(), m_lblScore(m_font), m_txtScore(m_font), m_lblHighScore(m_font), m_txtHighScore(m_font) {
     mapGrid = {
         "############################", 
         "#............##............#", 
@@ -81,6 +81,37 @@ Map::Map(): blinky(GhostType::BLINKY), pinky(GhostType::PINKY), inky(GhostType::
         }
     }
 
+    // Initialize UI
+    if (!m_font.openFromFile("assets/emulogic.ttf")) {
+        // Try fallback
+        if (!m_font.openFromFile("assets/font.ttf")) {
+             std::cerr << "Warning: No font found (assets/emulogic.ttf or assets/font.ttf)" << std::endl;
+        }
+    }
+
+    m_lblScore.setFont(m_font);
+    m_lblScore.setString("Your Score");
+    m_lblScore.setCharacterSize(22);
+    m_lblScore.setFillColor(sf::Color::White);
+    m_lblScore.setPosition(sf::Vector2f(35.f, 30.f));
+
+    m_txtScore.setFont(m_font);
+    m_txtScore.setString("0");
+    m_txtScore.setCharacterSize(22);
+    m_txtScore.setFillColor(sf::Color::White);
+    m_txtScore.setPosition(sf::Vector2f(35.f, 50.f));
+
+    m_lblHighScore.setFont(m_font);
+    m_lblHighScore.setString("High Score");
+    m_lblHighScore.setCharacterSize(22);
+    m_lblHighScore.setFillColor(sf::Color::White);
+    m_lblHighScore.setPosition(sf::Vector2f(250.f, 30.f));
+
+    m_txtHighScore.setFont(m_font);
+    m_txtHighScore.setString("0"); 
+    m_txtHighScore.setCharacterSize(22);
+    m_txtHighScore.setFillColor(sf::Color::White);
+    m_txtHighScore.setPosition(sf::Vector2f(250.f, 50.f));
 }
 
 void Map::draw(sf::RenderWindow& window) {
@@ -119,15 +150,46 @@ void Map::draw(sf::RenderWindow& window) {
             }
         }
     }
+
+    // Draw UI
+    window.draw(m_lblScore);
+    window.draw(m_txtScore);
+    window.draw(m_lblHighScore);
+    window.draw(m_txtHighScore);
 }
 
 void Map::update() {
+
+    // Pellet Interaction
+    sf::Vector2f pos = pacman.getPosition();
+    // Calculate center of Pacman (logic is top-left)
+    float centerX = pos.x + 8.0f; 
+    float centerY = pos.y + 8.0f;
+    
+    int gridX = static_cast<int>((centerX - gridOriginX) / itemSize);
+    int gridY = static_cast<int>((centerY - gridOriginY) / itemSize);
+    
+    if (gridX >= 0 && gridX < MAP_WIDTH && gridY >= 0 && gridY < MAP_HEIGHT) {
+        char cell = mapGrid[gridY][gridX];
+        if (cell == '.') {
+            mapGrid[gridY][gridX] = ' ';
+            m_score += 10;
+            m_txtScore.setString(std::to_string(m_score));
+        } else if (cell == 'o') {
+            mapGrid[gridY][gridX] = ' ';
+            m_score += 50;
+            m_txtScore.setString(std::to_string(m_score));
+            // TODO: Trigger frightened mode
+        }
+    }
+
     float dt = 1.0f / 60.0f; // Delta time fixe pour l'instant
     pacman.update(dt, mapGrid);
     blinky.update(dt, mapGrid);
     pinky.update(dt, mapGrid);
     inky.update(dt, mapGrid);
     clyde.update(dt, mapGrid);
+
 }
 
 void Map::handleInput(sf::Keyboard::Key key) {
