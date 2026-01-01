3aa7982824f220ec82620f6351b668a04973c895
Alexis Marzolin <163201587+AlexisMrz@users.noreply.github.com>
2026-01-02 00:29:58 +0100

    ajout de la gestion des vies de pacman
    Added methods to manage Pacman's lives.

--- include/Pacman.hpp
+++ include/Pacman.hpp
@@ -7,4 +7,11 @@ public:
     void draw(sf::RenderWindow& window) override;
     void update(float dt, const std::vector<std::string>& map) override;
     void setPosition(float x, float y) override;
+    
+    void removeLife() { lives--; }
+    int getLives() const { return lives; }
+    void resetLives() { lives = 3; }
+    
+private:
+    int lives = 3;
 };
